1853532456e72ebdcdfa9a574c9fc1372acc4033
Brati Pradhan <pradhanbrati@gmail.com>
2022-06-21 11:15:08 +0530

    DAY 7 | UNIQUE PATHS | RECURSION + DP

---
+++ UniquePaths.cpp
@@ -0,0 +1,23 @@
+// RECURSION + DP
+
+#include <bits/stdc++.h>
+
+int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp)
+{
+    if(i == m-1 && j == n-1) return 1;
+    if(i >= m || j >= n ) return 0;
+    
+    if(dp[i][j] != -1) return dp[i][j];
+    else return dp[i][j] = countPaths(i+1,j, m,n,dp) + countPaths(i, j+1,m,n,dp);
+
+    
+}
+
+int uniquePaths(int m, int n) {
+	vector<vector<int>> dp(m, vector<int> (n,-1));
+    
+    int i= 0, j = 0;
+    
+    int ans = countPaths(i, j, m, n, dp);
+    return ans;
+}
