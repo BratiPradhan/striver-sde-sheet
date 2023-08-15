320ad88074a9861fc1cedfe7e32858662285a572
Brati Pradhan <pradhanbrati@gmail.com>
2023-08-15 12:19:13 +0530

    merge two sorted arrays | gap method

---
+++ merge_two_sorted_arrays/Optimal2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int len = n+m;
+        int gap = (len / 2) + (len % 2);
+
+        for(int i = 0; i < n ; i++){
+            nums1[m+i] = nums2[i];
+        }
+
+        while(gap > 0){
+            int left = 0;
+            int right = gap;
+
+            while(right < len){
+                if(nums1[left] > nums1[right]){
+                    swap(nums1[left], nums1[right]);
+                }
+
+                left++;
+                right++;
+            }
+
+            if(gap == 1) break;
+            gap = (gap /2) + (gap % 2);
+        }
+
+        
+        
+    }
+};
